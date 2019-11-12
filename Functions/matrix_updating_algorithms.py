11f9084ee4b92463db44d2ea19ce8df6b5a08f8b
Rivera-py <jaluri@outlook.com>
2019-11-12 14:20:51 +0000

    Added the method which checks for unique cell possibilities.
    Signed-off-by: Rivera-py <jaluri@outlook.com>

--- Functions/matrix_updating_algorithms.py
+++ Functions/matrix_updating_algorithms.py
@@ -31,8 +31,8 @@ def update_column(update_number, column_index, matrix):
 def update_grid(update_number, row_index, column_index, matrix):
     grid_row_start = 3 * int(row_index / 3)
     grid_column_start = 3 * int(column_index / 3)
-    for grid_row in range(grid_row_start, grid_row_start + 3, 1):
-        for grid_column in range(grid_column_start, grid_column_start + 3, 1):
+    for grid_row in range(grid_row_start, grid_row_start + 3):
+        for grid_column in range(grid_column_start, grid_column_start + 3):
             if isinstance(matrix[grid_row][grid_column], list):
                 if update_number in matrix[grid_row][grid_column]:
                     matrix[grid_row][grid_column].remove(update_number)
@@ -54,4 +54,65 @@ def scan_and_solve(matrix):
                 solve_cell(row_index, column_index, matrix)
 
 
+# If a cell has only one possibility, it must be that number
+def one_possibility(matrix):
+    for row in matrix:
+        for column_index, elt in enumerate(row):
+            if isinstance(elt, list):
+                if len(elt) == 1:
+                    row[column_index] = row[column_index][0]
 
+
+def unique_row_possibility(matrix):
+    cell_solution_possible = False
+    for row in matrix:
+        for column_index, cell in enumerate(row):
+            if isinstance(cell, list):
+                for possibility in cell:
+                    counter = 0
+                    for check_column_index, checking_cell in enumerate(row):
+                        if isinstance(checking_cell, list):
+                            if possibility in checking_cell:
+                                counter += 1
+                    if counter == 1:
+                        row[column_index] = possibility
+                        cell_solution_possible = True
+                        break
+    return cell_solution_possible
+
+
+def unique_column_possibility(matrix):
+    cell_solution_possible = False
+    for row_index, row in enumerate(matrix):
+        for column_index, cell in enumerate(row):
+            if isinstance(cell, list):
+                for possibility in cell:
+                    counter = 0
+                    for check_row_index, checking_row in enumerate(matrix):
+                        if isinstance(checking_row[column_index], list):
+                            if possibility in checking_row[column_index]:
+                                counter += 1
+                    if counter == 1:
+                        row[column_index] = possibility
+                        cell_solution_possible = True
+                        break
+    return cell_solution_possible
+
+
+def unique_grid_possibility(matrix):
+    for row_index, row in enumerate(matrix):
+        for column_index, cell in enumerate(row):
+            if isinstance(cell, list):
+                for possibility in cell:
+                    counter = 0
+                    grid_row_start = 3 * int(row_index / 3)
+                    grid_column_start = 3 * int(column_index / 3)
+                    for grid_row in range(grid_row_start, grid_row_start + 3):
+                        for grid_column in range(grid_column_start, grid_column_start + 3):
+                            if isinstance(matrix[grid_row][grid_column], list):
+                                if (grid_row, grid_column) != (row_index, column_index):
+                                    counter += 1
+                    if counter == 1:
+                        row[column_index] = possibility
+                        return True
+    return False
