38d3ab75dd68dd7b6b5e87ad34f139e9fa371f04
Rivera-py <jaluri@outlook.com>
2019-11-12 14:21:37 +0000

    Created a more advanced solving algorithm
    Signed-off-by: Rivera-py <jaluri@outlook.com>

--- Functions/solving_algorithms.py
+++ Functions/solving_algorithms.py
@@ -1,4 +1,4 @@
-from Functions import matrix_updating_algorithms, test_sudokus, display_algorithms
+from Functions import matrix_updating_algorithms, test_sudokus, display_algorithms, checking_algorithms
 import copy
 
 
@@ -11,3 +11,17 @@ def simple_sudoku_solve(matrix):
         copy_matrix = copy.deepcopy(matrix)
         matrix_updating_algorithms.one_possibility(matrix)
         matrix_updating_algorithms.scan_and_solve(matrix)
+
+
+# Adds the unique possibility method
+def simple_sudoku_solve2(matrix):
+    run = True
+    while run:
+        simple_sudoku_solve(matrix)
+        if matrix_updating_algorithms.unique_row_possibility(matrix):
+            continue
+        if matrix_updating_algorithms.unique_column_possibility(matrix):
+            continue
+        if matrix_updating_algorithms.unique_grid_possibility(matrix):
+            continue
+        run = False
