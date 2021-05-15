84b0e220c391994c5662d60a8bcce6bce47b83e4
Jack Rivera <jaluri@outlook.com>
2021-05-15 17:23:12 +0000

    Adding a decode string

--- modules/sudoku.py
+++ modules/sudoku.py
@@ -1,4 +1,14 @@
-"""Module with an object holding a suite of attributes and methods to help with Sudoku solution."""
+"""
+Module with an object holding a suite of attributes and methods to help with Sudoku solution.
+
+Required:
+  - Stated imports
+  - Instantiated logger
+"""
+
+
+import json
+
 
 class Sudoku()
     """Sudoku object class."""
@@ -6,5 +16,11 @@ class Sudoku()
     def __init__(self, encoded_sudoku, logger=""):
         """Constructor, initialising Sudoku object."""
 
-        self.sudoku = self.decode_json(encoded_sudoku)
+        self.sudoku_rows = self.decode_json(encoded_sudoku)
         self.log = logger
+
+    @staticmethod
+    def decode_json(json_string):
+        """Turns JSON string dump into python readable data."""
+
+        return json.load(json_string)
